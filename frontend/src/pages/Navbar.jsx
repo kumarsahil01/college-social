d9ef0785fb6d4a2d2607f1ab33380aaf721bf664
sahil kumar <sahilkgandash@gmail.com>
2023-07-12 11:03:10 +0530

    testing

---
+++ frontend/src/pages/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  return (
+    <div className='Navbar'>
+        <img src="" alt="" />
+       <input type="text" />
+       <button>register</button>
+       <button>Login</button>
+    </div>
+  )
+}
+
+export default Navbar
