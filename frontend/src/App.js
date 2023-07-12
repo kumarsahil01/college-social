f099b323c1475ea059cbb517428f0c25a245a4ae
sahil kumar <sahilkgandash@gmail.com>
2023-07-12 09:57:00 +0530

    cheking

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn with sahil kumar
+          Learn with sahil
         </a>
       </header>
     </div>
